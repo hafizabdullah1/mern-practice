1108c0d6c5191a1779f4af7ab1c22b1ff0f10fed
Hafiz Abdullah <127143084+hafizabdullah1@users.noreply.github.com>
2025-11-02 14:47:59 +0500

    Update script.js

--- backend/script.js
+++ backend/script.js
@@ -82,7 +82,7 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-  console.log("Second Middleware executed");
+  console.log("Second Middleware executed.");
   next();
 });
 
@@ -106,3 +106,6 @@ app.use((err, req, res, next) => {
 });
 
 app.listen(5000);
+
+
+
